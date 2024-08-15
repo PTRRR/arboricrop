03f3468ee43735a105d0ea57d49d9cca1dbc7618
PTRRR <pietro.alberti@bluewin.ch>
2024-08-15 18:04:00 +0200

    Implement landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="nav">
+	<h1>Arboricrop UX/UI Designs</h1>
+
+	<div class="links">
+		<a href="/mobile-wireframe">Mobile Wireframe</a>
+	</div>
+</div>
+
+<svelte:head>
+	<title>Arboricrop UX/UI Design</title>
+</svelte:head>
+
+<style>
+	.nav {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 2rem;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: column;
+	}
+
+	a {
+		font-size: 1.5rem;
+		color: black;
+	}
+</style>
