4fee692daab3c7820d30b0cec9db2403650ca59c
PTRRR <pietro.alberti@bluewin.ch>
2024-08-20 16:06:53 +0200

    Add getting started page

---
+++ src/components/wireframe/Pagination.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let count: number = 4;
+</script>
+
+<div class="pagination">
+	{#each { length: count } as _}
+		<div class="pagination__bullet"></div>
+	{/each}
+</div>
+
+<style>
+	.pagination {
+		display: flex;
+		gap: var(--gap);
+	}
+
+	.pagination__bullet {
+		width: 5px;
+		height: 5px;
+		border-radius: 100%;
+		background-color: var(--dark-gray);
+	}
+</style>
--- src/routes/mobile-wireframe/getting-started/+page.svelte
+++ src/routes/mobile-wireframe/getting-started/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
+	import Spacer from '../../../components/Spacer.svelte';
+	import Button from '../../../components/wireframe/Button.svelte';
+	import Image from '../../../components/wireframe/Image.svelte';
+	import Pagination from '../../../components/wireframe/Pagination.svelte';
 	import { useReturnButton } from '../../../stores';
 
-	let returnButton = useReturnButton();
+	const returnButton = useReturnButton();
 	returnButton.set({
 		label: 'Getting Started',
 		href: '/mobile-wireframe'
 	});
 </script>
 
-<h1>Getting Started</h1>
+<div class="getting-started">
+	<Image ratio={1} placeholder="Slides / animations" />
+	<Spacer />
+	<Pagination />
+	<Spacer />
+	<Spacer />
+	<p>Description...</p>
+</div>
+
+<style>
+	.getting-started {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1 1 auto;
+		text-align: center;
+	}
+</style>
